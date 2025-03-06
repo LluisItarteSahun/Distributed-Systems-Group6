680132198fe3e32f3971cd956efe8666fb7c552e
Pau <pau.puigdomenech@estudiantat.upc.edu>
2025-03-06 11:37:34 +0100

    hellow pau

--- src/main/java/be/uantwerpen/fti/se/Main.java
+++ src/main/java/be/uantwerpen/fti/se/Main.java
@@ -2,6 +2,6 @@ package be.uantwerpen.fti.se;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, pau!");
     }
 }
